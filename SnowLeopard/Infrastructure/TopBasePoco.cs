01a9b3008f81ed1f22c3602e1ae1427bebc5ecae
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-05-08 11:26:43 +0800

    add 添加基类  Changes to be committed: 	new file:   SnowLeopard/Infrastructure/TopBasePoco.cs

---
+++ SnowLeopard/Infrastructure/TopBasePoco.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnowLeopard.Infrastructure
+{
+    /// <summary>
+    /// TopBasePoco
+    /// </summary>
+    public abstract class TopBasePoco
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Display(Name = "Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+
+    /// <summary>
+    /// TopBasePocoGuid
+    /// </summary>
+    public abstract class TopBasePocoGuid
+    {
+        private Guid _id;
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Display(Name = "Id")]
+        [Key]
+        public Guid Id
+        {
+            get
+            {
+                if (_id == Guid.Empty)
+                {
+                    _id = Guid.NewGuid();
+                }
+                return _id;
+            }
+        }
+    }
+}
