c9d99ca8977077a0c986b9f0f7ac0a1a816996df
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-07-03 11:36:01 +0800

    add 添加健康检查接口  Changes to be committed: 	new file:   src/SnowLeopard/Infrastructure/Controller/HealthController.cs

---
+++ src/SnowLeopard/Infrastructure/Controller/HealthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace SnowLeopard.Infrastructure
+{
+    [Produces("application/json")]
+    [Route("api/v1/[controller]")]
+    [AllowAnonymous]
+    public class HealthController : BaseApiController
+    {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// HealthController
+        /// </summary>
+        /// <param name="loggerFactory"></param>
+        public HealthController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HealthController>();
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation($"时间：{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+            return "ok";
+        }
+    }
+}
