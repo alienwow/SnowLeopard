dae692680361d7d070a8a5ea4bddc96c5776ced7
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-11-01 18:12:20 +0800

    add 添加 SnowLeopard.Caching  Changes to be committed: 	modified:   SnowLeopard.sln 	modified:   sample/SnowLeopard.WebApi/SnowLeopard.WebApi.csproj 	new file:   src/SnowLeopard.Caching.Abstractions/ICacheProvider.cs 	new file:   src/SnowLeopard.Caching.Abstractions/SnowLeopard.Caching.Abstractions.csproj 	new file:   src/SnowLeopard.Caching.Redis/RedisCacheProvider.cs 	new file:   src/SnowLeopard.Caching.Redis/SnowLeopard.Caching.Redis.csproj

---
+++ src/SnowLeopard.Caching.Abstractions/ICacheProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SnowLeopard.Caching
+{
+    /// <summary>
+    /// ICacheProvider
+    /// </summary>
+    public interface ICacheProvider
+    {
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Get<T>(string key);
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        Task<T> GetAsync<T>(string key);
+
+        /// <summary>
+        /// Set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="timeSpan"></param>
+        void Set<T>(string key, T value, TimeSpan timeSpan);
+
+        /// <summary>
+        /// Set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
+        Task SetAsync<T>(string key, T value, TimeSpan timeSpan);
+    }
+}
---
+++ src/SnowLeopard.Caching.Redis/RedisCacheProvider.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SnowLeopard.Redis;
+
+namespace SnowLeopard.Caching.Redis
+{
+    /// <summary>
+    /// RedisCacheProvider
+    /// </summary>
+    public class RedisCacheProvider : ICacheProvider
+    {
+        private readonly ILogger _logger;
+        private readonly IRedisCache _cache;
+
+        public RedisCacheProvider(
+            ILogger<RedisCacheProvider> logger,
+            IRedisCache cache
+        )
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T Get<T>(string key)
+        {
+            return _cache.Get<T>(key);
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(string key)
+        {
+            return await _cache.GetAsync<T>(key);
+        }
+
+        /// <summary>
+        /// Set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="timeSpan"></param>
+        public void Set<T>(string key, T value, TimeSpan timeSpan)
+        {
+            _cache.Set(key, value, timeSpan: timeSpan);
+        }
+
+        /// <summary>
+        /// Set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
+        public async Task SetAsync<T>(string key, T value, TimeSpan timeSpan)
+        {
+            await _cache.SetAsync(key, value, timeSpan: timeSpan);
+        }
+    }
+}
