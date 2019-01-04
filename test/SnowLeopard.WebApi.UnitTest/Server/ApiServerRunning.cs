5ffe714631026792fb42077b9a6c7aabc3ff97d8
Wenhao.Wu <wuwenhao0327@gmail.com>
2019-01-04 13:53:22 +0800

    dependency dotnetcore 升级到 v2.2.0  Changes to be committed: 	modified:   common.props 	modified:   sample/SnowLeopard.WebApi/SnowLeopard.WebApi.csproj 	modified:   sample/SnowLeopard.WebApi/appsettings.json 	modified:   src/SnowLeopard.Caching.Abstractions/SnowLeopard.Caching.Abstractions.csproj 	modified:   src/SnowLeopard.Caching.Redis/SnowLeopard.Caching.Redis.csproj 	modified:   src/SnowLeopard.FastDFS/SnowLeopard.FastDFS.csproj 	modified:   src/SnowLeopard.Lynx/SnowLeopard.Lynx.csproj 	modified:   src/SnowLeopard.Mongo/SnowLeopard.Mongo.csproj 	modified:   src/SnowLeopard.Redis/SnowLeopard.Redis.csproj 	modified:   src/SnowLeopard/SnowLeopard.csproj 	modified:   test/SnowLeopard.Console/SnowLeopard.Console.csproj 	modified:   test/SnowLeopard.Lynx.ConsoleTest/SnowLeopard.Lynx.ConsoleTest.csproj 	modified:   test/SnowLeopard.Lynx.UnitTest/SnowLeopard.Lynx.UnitTest.csproj 	modified:   test/SnowLeopard.Redis.ConsoleTest/SnowLeopard.Redis.ConsoleTest.csproj 	modified:   test/SnowLeopard.WebApi.UnitTest/Server/ApiServerRunning.cs 	modified:   test/SnowLeopard.WebApi.UnitTest/SnowLeopard.WebApi.UnitTest.csproj

--- test/SnowLeopard.WebApi.UnitTest/Server/ApiServerRunning.cs
+++ test/SnowLeopard.WebApi.UnitTest/Server/ApiServerRunning.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace SnowLeopard.WebApi.UnitTest
@@ -15,12 +16,13 @@ namespace SnowLeopard.WebApi.UnitTest
 
         public void GivenRunningOn(string url)
         {
-            _builder = new WebHostBuilder()
-                   .UseUrls(url)
-                   .UseKestrel()
-                   .UseContentRoot(Directory.GetCurrentDirectory())
-                   .UseStartup<Startup>()
-                   .Build();
+            _builder = WebHost
+                        .CreateDefaultBuilder()
+                        .UseUrls(url)
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseStartup<Startup>()
+                        .Build();
             _builder.Start();
         }
     }
