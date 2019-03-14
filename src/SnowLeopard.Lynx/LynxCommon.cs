b9e4c057605e846f02648f9aa9c64600d5935cd0
Vito <wuwenhao0327@gmail.com>
2019-03-14 13:26:13 +0800

    add 添加洗牌算法  Changes to be committed: 	new file:   src/SnowLeopard.Lynx/LynxCommon.cs 	modified:   version.props

---
+++ src/SnowLeopard.Lynx/LynxCommon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnowLeopard.Lynx
+{
+    /// <summary>
+    /// Lynx common extensions.
+    /// </summary>
+    public static class LynxCommonExtensions
+    {
+        #region 洗牌算法
+
+        /// <summary>
+        /// 洗牌算法
+        /// Shuffle the specified input.
+        /// </summary>
+        /// <returns>The shuffle.</returns>
+        /// <param name="input">Input.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static T[] Shuffle<T>(this T[] input)
+        {
+            var rand = new Random();
+            for (var i = input.Length - 1; i >= 0; i--)
+            {
+                var randomIndex = rand.Next(0, i + 1);
+                var itemAtIndex = input[randomIndex];
+
+                input[randomIndex] = input[i];
+                input[i] = itemAtIndex;
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// 洗牌算法
+        /// Shuffle the specified input.
+        /// </summary>
+        /// <returns>The shuffle.</returns>
+        /// <param name="input">Input.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static List<T> Shuffle<T>(this List<T> input)
+        {
+            var rand = new Random();
+            for (var i = input.Count - 1; i >= 0; i--)
+            {
+                var randomIndex = rand.Next(0, i + 1);
+                var itemAtIndex = input[randomIndex];
+
+                input[randomIndex] = input[i];
+                input[i] = itemAtIndex;
+            }
+            return input;
+        }
+
+        #endregion
+    }
+}
